242bd27dc8ac4a1c65bbd513ea5504b6fcd1a862
AlejoG1996 <89146947+AlejoG1996@users.noreply.github.com>
2023-02-01 19:27:00 -0500

    CRUD ACCOUNTTYPE

---
+++ AhorroDigital.API/Controllers/AccountTypesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AhorroDigital.API.Data;
+using AhorroDigital.API.Data.Entities;
+using Core.Flash;
+using Vereyon.Web;
+
+namespace AhorroDigital.API.Controllers
+{
+    public class AccountTypesController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly IFlashMessage _flashMessage;
+
+        public AccountTypesController(DataContext context, IFlashMessage flasher)
+        {
+            _context = context;
+            _flashMessage = flasher;
+        }
+
+      
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AccountTypes.ToListAsync());
+        }
+
+       
+
+       
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create( AccountType accountType)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(accountType);
+                    await _context.SaveChangesAsync();
+                    _flashMessage.Info(string.Empty, "Se registro exitosamente el  tipo de cuenta bancaria.");
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+
+                    if (dbUpdateException.InnerException.Message.Contains("Duplicate"))
+                    {
+                        _flashMessage.Danger(string.Empty, "Ya existe este tipo de cuenta bancaria.");
+
+                    }
+                    else
+                    {
+                        _flashMessage.Danger(string.Empty, dbUpdateException.InnerException.Message);
+
+
+                    }
+                }
+                catch (Exception exception)
+                {
+                    _flashMessage.Danger(string.Empty, exception.Message);
+
+
+                }
+            }
+
+            return View(accountType);
+        }
+
+       
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.AccountTypes == null)
+            {
+                return NotFound();
+            }
+
+            AccountType accountType = await _context.AccountTypes.FindAsync(id);
+            if (accountType == null)
+            {
+                return NotFound();
+            }
+            return View(accountType);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, AccountType accountType)
+        {
+            if (id != accountType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(accountType);
+                    await _context.SaveChangesAsync();
+                    _flashMessage.Info(string.Empty, "Se editó exitosamente el  tipo de cuenta bancaria.");
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+
+                    if (dbUpdateException.InnerException.Message.Contains("Duplicate"))
+                    {
+                        _flashMessage.Danger(string.Empty, "Ya existe este tipo de cuenta bancaria.");
+
+                    }
+                    else
+                    {
+                        _flashMessage.Danger(string.Empty, dbUpdateException.InnerException.Message);
+
+
+                    }
+                }
+                catch (Exception exception)
+                {
+                    _flashMessage.Danger(string.Empty, exception.Message);
+
+
+                }
+
+            }
+            return View(accountType);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            AccountType accountType = await _context.AccountTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (accountType == null)
+            {
+                return NotFound();
+            }
+
+            _context.AccountTypes.Remove(accountType);
+            await _context.SaveChangesAsync();
+            _flashMessage.Info(string.Empty, "Se elimino exitosamente el  tipo de cuenta bancaria.");
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ AhorroDigital.API/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using AhorroDigital.API.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace AhorroDigital.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AccountType> AccountTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AccountType>().HasIndex(x => x.Name).IsUnique();
+        
+        }
+
+    }
+}
